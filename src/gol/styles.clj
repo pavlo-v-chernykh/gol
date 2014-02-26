cd310f59eca55c10600a8045b5057ff0828554ca
041616 <041616@gmail.com>
2014-02-27 00:40:25 +0200

    CSS animation was deleted

--- src/gol/styles.clj
+++ src/gol/styles.clj
@@ -7,78 +7,16 @@
 )
 
 
-(def cell-size 10)
-
-
-(defkeyframes cell-blinking
-  [:from {:opacity 1}]
-  [:50% {:opacity 0.6}]
-  [:to {:opacity 1}]
-)
-
-
-(defkeyframes cell-animation
-  [:form {
-    :width (percent 100)
-    :height (percent 100)
-    :border-radius (percent 50) }]
-
-  [:20% {
-    :width (percent 110)
-    :height (percent 110)
-    :border-radius [[(percent 47) (percent 100) (percent 60) (percent 81)]] }]
-
-  [:40% {
-    :width (percent 100)
-    :height (percent 100)
-    :border-radius [[(percent 188) (percent 40) (percent 206) (percent 80)]] }]
-
-  [:60% {
-    :width (percent 110)
-    :height (percent 110)
-    :border-radius [[(percent 81) (percent 99) (percent 66) (percent 75)]] }]
-
-  [:80% {
-    :width (percent 100)
-    :height (percent 100)
-    :border-radius [[(percent 71) (percent 33) (percent 63) (percent 47)]] }]
-
-  [:to {
-    :width (percent 100)
-    :height (percent 100)
-    :border-radius (percent 50) }]
-)
-
-
-(defkeyframes cell-core
-  [:form {
-    :left (percent 50)
-    :top (percent 50) }]
-
-  [:33% {
-    :left (percent 30)
-    :top (percent 60) }]
-
-  [:66% {
-    :left (percent 35)
-    :top (percent 40) }]
-
-  [:to {
-    :left (percent 50)
-    :top (percent 50) }]
-)
+(def cell-size 12)
 
 
 (defstyles styles
-  cell-animation
-  cell-blinking
-  cell-core
-
   [:html {
     :height (percent 100) }]
 
   [:body {
     :display "table"
+    :min-width (px 980)
     :width (percent 100)
     :height (percent 100)
     :margin 0
@@ -100,17 +38,15 @@
     [:&:hover
       [:b {:border-color (hex->rgb "#ccc")}] ]
 
-    [:.cell ^:prefix {
-      :animation [[cell-blinking (s 2) "infinite" "linear"]] }]
-
     [:b {
       :display "inline-block"
       :width (px cell-size)
       :height (px cell-size)
       :padding (px 1)
       :margin [[(px -1) 0 0 (px -1)]]
+      :border [[(px 1) "dotted" "transparent"]]
       :vertical-align "top"
-      :border [[(px 1) "dotted" "transparent"]] }
+      :white-space "nowrap" }
 
       [:i {
         :position "relative"
@@ -124,9 +60,6 @@
           :border-radius (percent 50)
           :box-shadow [[0 0 (px 10) (rgba 0 0 0 0.2)]] }]
 
-        [:& ^:prefix {
-          :animation [[cell-animation (s 5) "infinite" "linear"]] }]
-
         [:&:after {
           :position "absolute"
           :top (percent 50)
@@ -137,8 +70,5 @@
           :content "\"\""}
           [:& ^:prefix {
             :border-radius (percent 50)
-            :box-shadow [[0 0 (px 3) (rgba 0 0 0 0.5)]] }]
-
-          [:& ^:prefix {
-            :animation [[cell-core (s 3) "infinite" "linear"]] }] ]]]]
+            :box-shadow [[0 0 (px 3) (rgba 0 0 0 0.5)]] }] ]]]]
 )
